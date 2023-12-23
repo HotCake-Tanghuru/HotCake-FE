7de1dcfc58b3d4d0a7679e775464fae27700966e
nayeongdev <nayeongkim.dev@gmail.com>
2023-12-24 02:13:34 +0900

    :sparkles: Feat: 스탬프 모달창 추가
    각 스탬프를 누르면 모달창이 표시되는 기능 추가

--- src/js/my_stamp.js
+++ src/js/my_stamp.js
@@ -11,3 +11,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+var modal = document.getElementById("stampModal");
+var closeBtn = document.querySelector(".close");
+
+// stamp 클래스를 가진 모든 요소에 대해 클릭 이벤트를 등록합니다.
+var stamps = document.querySelectorAll(".stamp");
+stamps.forEach(function (stamp) {
+  stamp.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+});
+
+// 닫기 버튼을 눌렀을 때의 이벤트를 등록합니다.
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// 모달창 외부를 클릭했을 때의 이벤트를 등록합니다.
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
