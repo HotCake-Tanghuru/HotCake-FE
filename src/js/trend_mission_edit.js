d2f335ab749074258ee85ec215693df5f46e8d56
nayeongdev <nayeongkim.dev@gmail.com>
2023-12-26 02:02:30 +0900

    :recycle: Refactor: 트렌드 미션 수정 레이아웃 및 스타일 수정
    trend_mission_detail로 파일명 변경, 사진 미리보기, 제거 버튼 추가
    
    - 수정 내용
     - edit_shot에서 trend_mission_detail로 파일 이름 변경
     - 사진 미리보기 추가
     - 사진 제거 버튼 추가
     - textarea 커스텀

---
+++ src/js/trend_mission_edit.js
@@ -0,0 +1,24 @@
+function previewImage() {
+  const input = document.querySelector('#imageUpload');
+  if (input.files && input.files[0]) {
+    const file = input.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      document.querySelector('#imagePreview').style.backgroundImage = `url(${e.target.result})`;
+    }
+    reader.readAsDataURL(file);
+  }
+}
+
+function removeSelectedFile() {
+  const input = document.querySelector('#imageUpload');
+  const preview = document.querySelector('#imagePreview');
+  
+  input.value = '';
+  preview.style.backgroundImage = 'none';
+}
+
+function trendMissionEdit(event) {
+  event.preventDefault();
+  // 나머지 코드...
+}
