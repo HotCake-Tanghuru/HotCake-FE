eb4095e4684968455a403d6fd6d20deb19b19ae2
nayeongdev <nayeongkim.dev@gmail.com>
2023-12-28 00:30:29 +0900

    :sparkles: Feat: 모든 페이지의 header의 사용자 이미지와 닉네임 추가
    - 토큰값을 받아오지 못해 main.js에 localStroage하는 부분 변경
    - 모든 페이지의 header에서 사용자 이미지와 닉네임을 불러옴(scripts)
    - scripts를 갖는 모든 페이지는 token 변수로 엑세스 토큰 받기 가능
    - BASEURL 변수 설정, scripts를 갖는 모든 페이지는 BASEURL변수로 값 요청
      가능

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,10 @@ console.log(data);
 
 access_token = data['access_token'];
 
-localStorage.setItem('access_tokens', JSON.stringify(data.access_token));
-localStorage.setItem('refresh_tokens', JSON.stringify(data.refresh_token));
+localStorage.setItem('access_tokens', data.access_token);
+localStorage.setItem('refresh_tokens', data.refresh_token);
 
 // 접속중인 사용자 확인
-
 fetch('http://43.202.230.2/users/info', {
   method: 'GET',
   headers: {
